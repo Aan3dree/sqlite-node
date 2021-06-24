c3c8d79901f646a289ebee54a5becbd3423b3d27
André Luiz <an3dree@gmail.com>
2021-06-24 09:37:54 -0300

    retrive all or sigle row

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database('escola.db', sqlite3.OPEN_READWRITE,(err) => {
+//open the database
+let db = new sqlite3.Database('C:/Users/andre.silva.de.souza/escola.db', sqlite3.OPEN_READWRITE,(err) => {
     if (err){
         return console.log(err.message);
     }
     console.log('Connected to the SQlite database');
 });
+
+//db.run(`INSERT INTO alunos VALUES(3, 'João Luiz', 'joao.luiz@gmail.com', '974364475')`);
+ 
+db.serialize(() => {
+    //console.log('Id' + '\t' + 'Nome');
+    db.each(
+        `SELECT id_aluno as id,
+            name as nome
+        FROM alunos    
+        `, (err, row) => {
+            if(err){
+                console.log(err.message);
+            }
+            console.log(row.id + "\t" + row.nome);
+        });
+});
+
 
 db.close((err) => {
     if (err){
