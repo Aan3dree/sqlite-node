90ac8a65549797b9d0b02b51ce45f887130dc127
André Luiz <an3dree@gmail.com>
2021-06-24 10:43:47 -0300

    state before API

--- profs.js
+++ profs.js
@@ -1,8 +1,16 @@
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('escola.db');
 
+db.run(`INSERT INTO professores VALUES
+    (2, 'Silva Holand', 'silva@holand.com', '988772244', 'Intenligencia Artificial'),
+    (3, 'Joseph Elis', 'joseph@elis.com', '978775542', 'Tibia'),
+    (4, 'Merlin Santos', 'merlin@santos.com', '977554466', 'Magia')`);
+
 //Retrieve all rows
 db.all('SELECT id_professor, name, email, telefone, disciplina FROM professores', (err, rows) => {
+    if (err){
+        return console.log(err.message);
+    }
     rows.forEach((row) => {
         console.log(
             row.id_professor +
